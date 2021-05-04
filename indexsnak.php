7b3dff958f145dffba7c6107f6365f7a82356074
titty91 <tznbaraldi@gmail.com>
2021-05-04 14:46:11 +0200

    nw file

---
+++ indexsnak.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    /* Snack 1Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario.
+    Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e
+    punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+    Olimpia Milano - Cantù | 55-60
+    */
+
+
+    /* Snack 2Passare come parametri GET name, mail e age e verificare (cercando i metodi che
+    non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
+    che mail contenga un punto e una chiocciola e che age sia un numero.
+    Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"
+    */
+    ?>
+  </body>
+</html>
