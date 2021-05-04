c132ad3747791260042c0bc0d6c1142eb133ca00
titty91 <tznbaraldi@gmail.com>
2021-05-04 15:20:59 +0200

    snack1

--- indexsnak.php
+++ indexsnak.php
@@ -5,13 +5,36 @@
     <title></title>
   </head>
   <body>
+    <h1>ciao</h1>
     <?php
     /* Snack 1Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario.
     Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e
     punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
     Olimpia Milano - Cantù | 55-60
     */
+    $basket = [
+      [
+        'home'=> 'teamA';
+        'Opposing' => 'teamB';
+        'pointsHome'=> '55';
+        'pointsOpp'=> '60';
+      ],
+      [
+        'home'=> 'teamC';
+        'Opposing' => 'teamD';
+        'pointsHome'=> '40';
+        'pointsOpp'=> '80';
+      ],
+      [
+        'home'=> 'teamE';
+        'Opposing' => 'teamF';
+        'pointsHome'=> '10';
+        'pointsOpp'=> '75';
+      ],
+    ];
 
+    echo $basket;
+    var_dump($basket);
 
     /* Snack 2Passare come parametri GET name, mail e age e verificare (cercando i metodi che
     non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
